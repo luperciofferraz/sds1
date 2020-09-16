ec98325f898c3d2cc5abbf99b0c4ed68ba58a2e4
Lupércio Fernando Ferraz <lupercio.ferraz@gmail.com>
2020-09-16 18:54:47 -0300

    Criando página Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <div>
+    
+      <Header />
 
-    <Header />
-
+      <Home />
+    
+    </div>
   );
 }
 
